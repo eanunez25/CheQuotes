3f2f8d8912e19dd6a05d367cc2506dfe79e47300
Elvin <eanunez25@gmail.com>
2021-08-02 09:26:00 -0500

    add quote

--- js.js
+++ js.js
@@ -2591,6 +2591,11 @@ let cheQuotes = [
 `<blockquote>
   For someone to win, someone gotta lose
 </blockquote>`,
+
+<`blockquote>
+  Maybe we do not lose our socks, maybe our socks get a divorce and the one you have is 
+  the one who got to keep you
+</blockquote>`,
 ]
 
 // categories(#used) ------------------------------------------------------------------------
