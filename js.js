312e02c5d936de7a56d652b9ccd61c71574c94ff
Elvin <eanunez25@gmail.com>
2022-12-22 09:58:49 -0600

    add quote

--- js.js
+++ js.js
@@ -4858,6 +4858,8 @@ Sex is like money; only too much is enough.
 </blockquote>P. J. O'Rourke`,
 
 `<img src="images/messi.jpg" class="image"></img>`,
+
+`<img src="images/zel.jpg" class="image"></img>`,
 ]
 
 
